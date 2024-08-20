96f1ddcfc656a896e4830380f7e58e726ca92d1d
MauricioCarranza2024 <wichovelasquez413@gmail.com>
2024-08-20 10:18:52 -0600

    mensaje

--- hola.py
+++ hola.py
@@ -1,4 +1,22 @@
-print("hola hola")
+class pelicula():
+    def __init__(self):
+        self.director = ""
+        self.genero = ""
+        self.anio = 0
+        self.duracion = 0
+        self.actores = []
+        self.descripcion = ""
 
+class humor():
+    def __init__(self, director, genero, duracion, actores):
+        self.pelicula = pelicula()
+        self.pelicula.director = director
+        self.pelicula.genero = genero
+        self.pelicula.duracion = duracion
+        self.pelicula.actores = actores
+        self.pelicula.descripcion = "Pelicula de humor colombiana."
 
-print("hola hola hola")
+class Colombiana():
+    def __init__(self):
+        self.peliculas = []
+        
